5ac387a287de1900367aa7c4abae71eae5a36c0c
nayarrab <nayarasalsabilla@gmail.com>
2024-11-19 14:06:20 +0700

    Create cisco.sh

---
+++ cisco.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CISCO_IP="192.168.27.2"
+CISCO_USER="admin"
+CISCO_PASS="0"
+
+CONFIG_COMMANDS="
+enable
+configure terminal
+vlan 10
+name Management
+exit
+interface e0/0
+switchport trunk encapsulation dot1q
+switchport mode trunk
+exit
+interface e0/1
+switchport mode access
+switchport access vlan 10
+exit
+write memory
+"
+
+sshpass -p "$CISCO_PASS" ssh -o StrictHostKeyChecking=no "$CISCO_USER@$CISCO
