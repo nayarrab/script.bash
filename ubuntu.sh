55e49e154bfaa5fb9eda2777e04044e50048a529
nayarrab <nayarasalsabilla@gmail.com>
2024-11-25 21:57:56 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -23,7 +23,6 @@ echo "======================================"
 
 set -e 
 
-echo "[1/4] Update Repository..."
 cat <<EOF | sudo tee /etc/apt/sources.list 
 deb http://kartolo.sby.datautama.net.id/ubuntu/ focal main restricted universe multiverse
 deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-updates main restricted universe multiverse
@@ -32,9 +31,8 @@ deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-backports main restricted 
 deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-proposed main restricted universe multiverse
 EOF
 
-sudo apt update /dev/null 2>&1
+sudo apt update 
 
-echo "[2/4] Configurig Ip Address..."
 cat <<EOF | sudo tee /etc/netplan/01-netcfg.yaml
 network:
   version: 2
@@ -52,10 +50,9 @@ network:
       addresses: [192.168.27.1/24]
 EOF
 
-sudo netplan apply /dev/null 2>&1
+sudo netplan apply
 
-echo "[3/4] Installing DHCP Server..."
-sudo apt install -y isc-dhcp-server iptables iptables-persistent /dev/null 2>&1
+sudo apt install -y isc-dhcp-server iptables iptables-persistent 
 
 cat <<EOF | sudo tee /etc/dhcp/dhcpd.conf
   # A slightly different configuration for an internal subnet.
@@ -71,17 +68,17 @@ cat <<EOF | sudo tee /etc/dhcp/dhcpd.conf
 EOF
 
 echo "[4/4] Restarting DHCP Server..."
-sudo /etc/init.d/isc-dhcp-server restart /dev/null 2>&1
+sudo /etc/init.d/isc-dhcp-server restart 
 
 sudo sed -i 's/^INTERFACES4=.*/INTERFACESV4="eth1.10"/' /etc/default/isc-dhcp-server
 
-sudo /etc/init.d/isc-dhcp-server restart /dev/null 2>&1
+sudo /etc/init.d/isc-dhcp-server restart 
 
-sudo apt install -y iptables /dev/null 2>&1
+sudo apt install -y iptables
 
-sudo sysctl -w net.ipv4.ip_forward=1 /dev/null 2>&1
+sudo sysctl -w net.ipv4.ip_forward=1
 echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf 
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE /dev/null 2>&1
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 sudo netfilter-persistent save
 
