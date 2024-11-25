e4aedb80af0c790f66f5f358b9003091060ee34b
nayarrab <nayarasalsabilla@gmail.com>
2024-11-25 21:26:09 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -7,25 +7,23 @@ fi
 
 clear
 
-cat << "EOF" | lolcat
-,---.   .--.   ____       ____     __   .-''-.   
-|    \  |  | .'  __ `.    \   \   /  /.'_ _   \  
-|  ,  \ |  |/   '  \  \    \  _. /  '/ ( ` )   ' 
-|  |\_ \|  ||___|  /  |     _( )_ .'. (_ o _)  | 
-|  _( )_\  |   _.-`   | ___(_ o _)' |  (_,_)___| 
-| (_ o _)  |.'   _    ||   |(_,_)'  '  \   .---. 
-|  (_,_)\  ||  _( )_  ||   `-'  /    \  `-'    / 
-|  |    |  |\ (_ o _) / \      /      \       /  
-'--'    '--' '.(_,_).'   `-..-'        `'-..-'   
-                                                  
-EOF
-
+echo ",---.   .--.   ____       ____     __   .-''-.   "
+echo "|    \  |  | .'  __ \`.    \   \   /  /.'_ _   \  "
+echo "|  ,  \ |  |/   '  \  \    \  _. /  '/ ( \` )   ' "
+echo "|  |\_ \|  ||___|  /  |     _( )_ .'. (_ o _)  | "
+echo "|  _( )_\  |   _.-\`   | ___(_ o _)' |  (_,_)___| "
+echo "| (_ o _)  |.'   _    ||   |(_,_)'  '  \   .---. "
+echo "|  (_,_)\  ||  _( )_  ||   \`-'  /    \  \`-'    / "
+echo "|  |    |  |\ (_ o _) / \      /      \       /  "
+echo "'--'    '--' '.(_,_).'   \`-..-'        \`'-..-'   "
+echo "                                                 "
 echo "======================================"
 echo " Starting DHCP Server Configuration... "
 echo "======================================"
 
 set -e 
 
+echo "[1/4] Update Repository..."
 cat <<EOF | sudo tee /etc/apt/sources.list 
 deb http://kartolo.sby.datautama.net.id/ubuntu/ focal main restricted universe multiverse
 deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-updates main restricted universe multiverse
@@ -34,8 +32,9 @@ deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-backports main restricted 
 deb http://kartolo.sby.datautama.net.id/ubuntu/ focal-proposed main restricted universe multiverse
 EOF
 
-sudo apt update 
+sudo apt update /dev/null 2>&1
 
+echo "[2/4] Configurig Ip Address..."
 cat <<EOF | sudo tee /etc/netplan/01-netcfg.yaml
 network:
   version: 2
@@ -53,9 +52,10 @@ network:
       addresses: [192.168.27.1/24]
 EOF
 
-sudo netplan apply 
+sudo netplan apply /dev/null 2>&1
 
-sudo apt install -y isc-dhcp-server iptables iptables-persistent
+echo "[3/4] Installing DHCP Server..."
+sudo apt install -y isc-dhcp-server iptables iptables-persistent /dev/null 2>&1
 
 cat <<EOF | sudo tee /etc/dhcp/dhcpd.conf
   # A slightly different configuration for an internal subnet.
@@ -70,17 +70,18 @@ cat <<EOF | sudo tee /etc/dhcp/dhcpd.conf
   }
 EOF
 
-sudo /etc/init.d/isc-dhcp-server restart 
+echo "[4/4] Restarting DHCP Server..."
+sudo /etc/init.d/isc-dhcp-server restart /dev/null 2>&1
 
 sudo sed -i 's/^INTERFACES4=.*/INTERFACESV4="eth1.10"/' /etc/default/isc-dhcp-server
 
-sudo /etc/init.d/isc-dhcp-server restart 
+sudo /etc/init.d/isc-dhcp-server restart /dev/null 2>&1
 
-sudo apt install -y iptables
+sudo apt install -y iptables /dev/null 2>&1
 
-sudo sysctl -w net.ipv4.ip_forward=1
+sudo sysctl -w net.ipv4.ip_forward=1 /dev/null 2>&1
 echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf 
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE 
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE /dev/null 2>&1
 
 sudo netfilter-persistent save
 
