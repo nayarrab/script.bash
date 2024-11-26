f996391aa930eb705edf9b2c4e9fda987d22280b
nayarrab <nayarasalsabilla@gmail.com>
2024-11-26 11:02:00 +0700

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -1,8 +1,3 @@
-alias do.dhcp='path/to/cisco.sh'
 alias do.dhcp='script.bash/ubuntu.sh'
-
-alias do.cisco='path/to/cisco.sh'
 alias do.cisco='script.bash/cisco.sh'
-
-alias do.mikrotik='path/to/cisco.sh'
 alias do.mikrotik='script.bash/mikrotik.sh'
