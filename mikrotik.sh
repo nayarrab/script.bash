92d630cebcea49fdd8c53a69dccaae088036f569
nayarrab <nayarasalsabilla@gmail.com>
2024-12-18 22:12:27 +0700

    Update mikrotik.sh

--- mikrotik.sh
+++ mikrotik.sh
@@ -16,35 +16,22 @@ echo "======================================"
 echo " Starting Mikrotik Configuration... "
 echo "======================================"
 
-IPNET="192.168.187.138"
+IPNET="192.168.187.132"
 MIKROTIK_IP="192.168.200.1"
 MIKROTIK_S="192.168.200.0"
 MPORT="30034"
 
 expect <<EOF > /dev/null 2>&1
 spawn telnet $IPNET $MPORT
-expect "Mikrotik Login:" {send "admin\r"}
-
-expect "Password:"
-send "\r"
-
-expect ">"
-send "n"
-
-expect "new password"
-send "0\r"
-
-expect "repeat new password"
-send "0\r"
-
-expect ">" 
-send "/ip address add address=192.168.200.1/24 interface=ether2\r"
-expect ">"
-send "/ip firewall nat add chain=srcnat out-interface=ether1 action=masquerade\r"
-expect ">"
-send "/ip route add gateway=192.168.27.1\r"
-expect ">"
-send "/ip dhcp-server setup\r"
+expect "Mikrotik Login:" { send "admin\r" }
+expect "Password:" { send "\r" }
+expect ">" { send "n" }
+expect "new password" { send "0\r" }
+expect "repeat new password" { send "0\r" }
+expect ">" { send "/ip address add address=192.168.200.1/24 interface=ether2\r" }
+expect ">" { send "/ip firewall nat add chain=srcnat out-interface=ether1 action=masquerade\r" }
+expect ">" { send "/ip route add gateway=192.168.27.1\r" }
+expect ">" { send "/ip dhcp-server setup\r" }
 expect "Select interface" { send "ether2\r" }
 expect "Select address pool" { send "dhcp_pool\r" }
 expect "Select gateway" { send "192.168.200.1\r" }
