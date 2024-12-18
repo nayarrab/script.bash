fcde0c819a1d3af8028e827be8daee1b660e1deb
nayarrab <nayarasalsabilla@gmail.com>
2024-12-18 21:25:09 +0700

    Update cisco.sh

--- cisco.sh
+++ cisco.sh
@@ -16,14 +16,14 @@ echo "======================================"
 echo " Starting Cisco Switch Configuration... "
 echo "======================================"
 
-CISCO_IP="192.168.187.138"
+CISCO_IP="192.168.187.132"
 CISCO_PORT="30032"
-expect << EOF > /dev/null 
+expect <<EOF > /dev/null 2>&1
 spawn telnet $CISCO_IP $CISCO_PORT
 set timeout 22
 
 expect ">" { send "enable\r" }
-expect "(config)#" { send "configure terminal\r" }
+expect "#" { send "configure terminal\r" }
 expect "(config)#" { send "vlan 10" }
 expect "(config-vlan)#" { send "exit\r" }
 expect "(config)#" { send "interface Ethernet0/1\r" }
